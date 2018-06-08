f4d095e308d8dd6e45084be66b9762a18633b751
Calvin Simran Rai <raicalvin@gmail.com>
2018-06-08 16:55:24 +0000

    automatically backed up by learn

---
+++ script.js
@@ -0,0 +1,10 @@
+//Enter querySelector Code 
+var header = document.querySelector('#header').innerHTML;
+var linkText = document.querySelector('a').innerHTML;
+var linkHref = document.querySelector('a');
+console.log('--- --- ---');
+
+// NO NEED TO MODIFY BELOW HERE
+console.log(`The header is: ${header}`)
+console.log(`The first link text is: ${linkText}`)
+console.log(`The first link href is: ${linkHref}`)
